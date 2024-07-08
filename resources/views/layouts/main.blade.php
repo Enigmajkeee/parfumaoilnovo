22409f907367676d68f00230a7f912ef77f2b638
enigmajkee <enigmajkee@gmail.com>
2024-07-08 12:18:07 +0300

    Commit1

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,47 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Edica :: Home</title>
-    <link rel="stylesheet" href="assets/vendors/flag-icon-css/css/flag-icon.min.css">
-    <link rel="stylesheet" href="assets/vendors/font-awesome/css/all.min.css">
-    <link rel="stylesheet" href="assets/vendors/aos/aos.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <script src="assets/vendors/jquery/jquery.min.js"></script>
-    <script src="assets/js/loader.js"></script>
+    <title>ParfumaOilNovo</title>
+    <link rel="stylesheet" href="{{ asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/font-awesome/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/aos/aos.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
+    <script src="{{ asset('assets/vendors/jquery/jquery.min.js')}}"></script>
+    <script src="{{ asset('assets/js/loader.js')}}"></script>
 </head>
 <body>
 <div class="edica-loader"></div>
 <header class="edica-header">
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light">
-            <a class="navbar-brand" href="index.html"><img src="assets/images/logo.svg" alt="Edica"></a>
+            <a class="navbar-brand" href="index.html"><img src="{{ asset('assets/images/logo.svg') }}" alt="Edica"></a>
             <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#edicaMainNav" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="edicaMainNav">
                 <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="index.html">Главная <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="about.html">About</a>
+                        <a class="nav-link" href="about.html">О нас</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="blogDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog</a>
+                        <a class="nav-link dropdown-toggle" href="#" id="blogDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Каталог</a>
                         <div class="dropdown-menu" aria-labelledby="blogDropdown">
-                            <a class="dropdown-item" href="blog.html">Blog Archive</a>
-                            <a class="dropdown-item" href="blog-single.html">Blog Post</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
-                        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-                            <a class="dropdown-item" href="404.html">404</a>
-                            <a class="dropdown-item" href="coming-soon.html">Coming Soon</a>
+                            <a class="dropdown-item" href="blog.html">Парфюмерные масла</a>
+                            <a class="dropdown-item" href="blog-single.html">Аксессуары</a>
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="contact.html">Contact</a>
+                        <a class="nav-link" href="contact.html">Контакты</a>
                     </li>
                 </ul>
                 <ul class="navbar-nav mt-2 mt-lg-0">
@@ -67,8 +60,8 @@
         <div class="footer-banner" data-aos="fade-up">
             <h1 class="banner-title">Download it now.</h1>
             <div class="banner-btns-wrapper">
-                <button class="btn btn-success"> <img src="assets/images/apple@1x.svg" alt="ios" class="mr-2"> App Store</button>
-                <button class="btn btn-success"> <img src="assets/images/android@1x.svg" alt="android" class="mr-2"> Google Play</button>
+                <button class="btn btn-success"> <img src="{{ asset('assets/images/apple@1x.svg') }}" alt="ios" class="mr-2"> App Store</button>
+                <button class="btn btn-success"> <img src="{{ asset('assets/images/android@1x.svg') }}" alt="android" class="mr-2"> Google Play</button>
             </div>
             <p class="banner-text">Add some helper text here to explain the finer details of your <br> product or service.</p>
         </div>
@@ -81,8 +74,8 @@
                 <a href="index.html" class="footer-brand-wrapper">
                     <img src="assets/images/logo.svg" alt="edica logo">
                 </a>
-                <p class="contact-details">hello@edica.com</p>
-                <p class="contact-details">+23 3000 000 00</p>
+                <p class="contact-details">olka050387@gmail.com</p>
+                <p class="contact-details">+375 29 119 73 30</p>
                 <nav class="footer-social-links">
                     <a href="#!"><i class="fab fa-facebook-f"></i></a>
                     <a href="#!"><i class="fab fa-twitter"></i></a>
@@ -136,10 +129,10 @@
         </div>
     </div>
 </footer>
-<script src="assets/vendors/popper.js/popper.min.js"></script>
-<script src="assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="assets/vendors/aos/aos.js"></script>
-<script src="assets/js/main.js"></script>
+<script src="{{ asset('assets/vendors/popper.js/popper.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/aos/aos.js') }}"></script>
+<script src="{{ asset('assets/js/main.js') }}"></script>
 <script>
     AOS.init({
         duration: 1000
